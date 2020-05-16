2193dbe912e043175e3ae198055c5025774bb5ba
Dunny <duapuy@gmail.com>
2020-05-15 22:48:09 -0600

    resolver tarea

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  list(
+    set = function(newX) {
+      x <<- newX
+      i <<- NULL
+    },
+    get = function()
+      x,
+    setinverse = function(newI)
+      i <<- newI,
+    getinverse = function()
+      i
+  )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    return(i)
+  }
+  i <- solve(x$get())
+  x$setinverse(i)
+  i
 }
